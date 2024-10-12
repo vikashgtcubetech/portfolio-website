5ff2e48577dc82d95509f15d7060cc5857470a4a
vikashgtcubetech <vikashsaini6640@gmail.com>
2024-10-12 08:00:10 +0530

    update

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+const menu = document.querySelector('#menu-icon');
+const navlist = document.querySelector('.navlist');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle("active")
+};
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove("active")
+};
+
+const sr = ScrollReveal({
+    distance:'45px',
+    duration:2700,
+    reset:true
+})
+sr.reveal('.home_text',{delay:150, origin:'left'});
+sr.reveal('.home_img',{delay:150, origin:'right'});
+
+sr.reveal('.sub-service,.about,.portfolio,.service,.contact',{delay:100, origin:'bottom'});
+
+
